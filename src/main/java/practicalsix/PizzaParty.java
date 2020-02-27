f4bbb553a79b991c05200660abc907d560f5ff66
Douglas Luman <dluman@douglasjluman.com>
2020-02-26 20:53:19 -0500

    Initial commit

---
+++ src/main/java/practicalsix/PizzaParty.java
@@ -0,0 +1,37 @@
+package practicalsix;
+
+/** Solves the pizza party problem.
+ * 
+ * @author Solutions Repository
+ */
+public class PizzaParty {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    /*
+     * TODO: Create fractions
+     */
+    
+    /*
+     * TODO: Use
+     *       - getNumer
+     *       - getDenom
+     *       to multiply the fractions
+     */
+    
+    /*
+     * TODO: Use 
+     *       - setNumer
+     *       - setDenom
+     *       to set the numerator/denominator of the appropriate fraction to the new values
+     */
+    
+    /*
+     * TODO: Print the output using
+     *       - getDecimalValue
+     */
+  }
+}
